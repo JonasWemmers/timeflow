e0fcb268699ddf1ca72431105eeeca24b9081093
Jonas Wemmers <jonas.wemmers@freenet.de>
2025-07-14 17:46:47 +0200

    chore: use firebase_options for Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
-import 'package:firebase_core/firebase_core.dart'; // Firebase Core importieren
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Firebase initialisieren
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, 
+  );
   runApp(const TimeFlowApp());
 }
 
