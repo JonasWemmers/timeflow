8335b47ce139a42b9b33d5d6a6362ede7b9962fc
Jonas Wemmers <jonas.wemmers@freenet.de>
2025-07-14 17:14:42 +0200

    Initial commit – TimeFlow starts here

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(const TimeFlowApp());
+
+class TimeFlowApp extends StatelessWidget {
+  const TimeFlowApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('Time Flow'),
+        ),
+        child: Center(
+          child: Text('Welcome to Time Flow!'),
+        ),
+      ),
+    );
+  }
+}
